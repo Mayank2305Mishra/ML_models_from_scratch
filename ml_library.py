6c15c43c997f38f4b430d3a526e81dab8e79ed1f
Mayank Mishra <mayank2305mishra@gmail.com>
2025-12-19 19:33:45 +0000

    Logistic_Regression + Dataset

--- ml_library.py
+++ ml_library.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
-from ml_metrics_eval import r2_score, mean_squared_error, root_mean_squared_error, mean_absolute_error
+from ml_metrics_eval import *
 # Creating a class for Linear Regression
 
 class LinearRegression:
-    def __init__(self, alpha=0.01, epochs=1000):
+    def __init__(self, alpha=0.01, iters=1000):
         self.alpha = alpha
-        self.epochs = epochs
+        self.iters = iters
         self.weights = None
         self.bias = None
         self.J_history = []
@@ -38,28 +38,28 @@ class LinearRegression:
     def gradient_descent(self, x, y):
         self.weights = np.zeros(x.shape[1])
         self.bias = 0
-        for epoch in range(self.epochs):
+        for iter in range(self.iters):
             dj_dw, dj_db = self.gradient_calculation(x, y)
             self.weights -= self.alpha * dj_dw
             self.bias -= self.alpha * dj_db
             cost = self.cost_function(x, y)
             self.J_history.append(cost)
-            if epoch% math.ceil(self.epochs / 10) == 0 :
-                print(f"Epoch {epoch}: Cost {cost}")
+            if iter% math.ceil(self.iters / 10) == 0 :
+                print(f"Epoch {iter}: Cost {cost}")
         return self.weights, self.bias
     def fit(self, x, y):
         self.weights, self.bias = self.gradient_descent(x, y)
         return self.weights, self.bias
     def plot_cost(self):
-        plt.plot(range(self.epochs), self.J_history)
-        plt.xlabel("No. of iterations (Epochs)")
+        plt.plot(self.J_history)
+        plt.xlabel("No. of iterations")
         plt.ylabel("Cost")
         plt.title("Cost vs iterations")
         plt.show()
     def plot_regression_line(self, x, y):
-        plt.scatter(y, y, color='blue', label='Data points')
-        plt.scatter(y, self.predict(x), color='red', label='Regression line')
-        plt.xlabel("X")
+        plt.scatter(x[:,0], y, color='blue', label='Data points')
+        plt.scatter(x[:,0], self.predict(x), color='red', label='Regression Model')
+        plt.xlabel("X-1st Column")
         plt.ylabel("Y")
         plt.title("Linear Regression Fit")
         plt.legend()
@@ -79,11 +79,11 @@ class LinearRegression:
         print(f"Weights: {self.weights}")
         print(f"Bias: {self.bias}")
         print(f"Learning Rate: {self.alpha}")
-        print(f"Epochs: {self.epochs}")
+        print(f"Iterations: {self.iters}")
         
 class PolynomialRegression(LinearRegression):
-    def __init__(self, degree=2, alpha=0.01, epochs=1000):
-        super().__init__(alpha, epochs)
+    def __init__(self, degree=2, alpha=0.01, iters=1000):
+        super().__init__(alpha, iters)
         self.degree = degree
     def polynomial_features_multi(self,x, degree):
         n_samples, n_features = x.shape
@@ -123,3 +123,108 @@ class PolynomialRegression(LinearRegression):
         plt.show()
     def evaluation_metrics(self, y_true, y_pred):
         return super().evaluation_metrics(y_true, y_pred)
+
+class LogisticRegression:
+    def __init__(self, alpha=0.01, iters=1000):
+        self.alpha = alpha
+        self.iters = iters
+        self.weights = None
+        self.bias = None
+        self.J_history = []
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    def cost_function(self, x, y):
+        m = len(x)
+        cost = 0
+        for i in range(m):
+            z = np.dot(x[i], self.weights) + self.bias
+            f_wb = self.sigmoid(z)
+            cost += - (y[i] * np.log(f_wb) + (1 - y[i]) * np.log(1 - f_wb))
+        return cost/m
+    def gradient_calculation(self, x, y):
+        m = len(x)
+        dj_dw = np.zeros(self.weights.shape)
+        dj_db = 0
+        for i in range(m):
+            z = np.dot(x[i], self.weights) + self.bias
+            f_wb = self.sigmoid(z)
+            err = f_wb - y[i]
+            for j in range(len(self.weights)):
+                dj_dw[j] += err * x[i][j]
+            dj_db += err
+        dj_dw /= m
+        dj_db /= m
+        return dj_dw, dj_db
+    def gradient_descent(self, x, y):
+        self.weights = np.zeros(x.shape[1])
+        self.bias = 0
+        for iter in range(self.iters):
+            dj_dw, dj_db = self.gradient_calculation(x, y)
+            self.weights -= self.alpha * dj_dw
+            self.bias -= self.alpha * dj_db
+            cost = self.cost_function(x, y)
+            self.J_history.append(cost)
+            if iter % math.ceil(self.iters / 10) == 0:
+                print(f"Iteration {iter}: Cost {cost}")
+        return self.weights, self.bias
+    def fit(self, x, y):
+        self.weights, self.bias = self.gradient_descent(x, y)
+        return self.weights, self.bias
+    def predict(self, X):
+        z = np.dot(X, self.weights) + self.bias
+        y_pred_proba = self.sigmoid(z)
+        return (y_pred_proba >= 0.5).astype(int)
+    def probability(self, X):
+        z = np.dot(X, self.weights) + self.bias
+        return self.sigmoid(z)
+    def evaluation_metrics(self, y_true, y_pred):
+        accuracy = accuracy_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        cm = confusion_matrix(y_true, y_pred)
+        print(f"Accuracy: {accuracy}")
+        print(f"Precision: {precision}")
+        print(f"Recall: {recall}")
+        print(f"F1 Score: {f1}")
+        print(f"Confusion Matrix:\n{cm}")
+        plt.imshow(cm, cmap='Blues')
+        plt.colorbar()
+        plt.xlabel("Predicted") 
+        plt.ylabel("Actual")
+        plt.title("Confusion Matrix")
+        return accuracy, precision, recall, f1, cm
+    def plot_cost(self):
+        plt.plot(self.J_history)
+        plt.xlabel("No. of iterations")
+        plt.ylabel("Cost")
+        plt.title("Cost vs iterations")
+        plt.show()
+    def plot_regression_line(self, x, y):
+        plt.scatter(y, y, color='blue', label='Data points')
+        plt.scatter(y, self.predict(x), color='red', label='Regression line')
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.title("Polynomial Regression Fit")
+        plt.legend()
+        plt.show()
+
+def train_test_split(X, y, test_size=0.2):
+    n_samples = len(X)
+    n_test = int(test_size * n_samples)
+    indices = np.arange(n_samples)
+    np.random.shuffle(indices)
+    test_indices = indices[:n_test]
+    train_indices = indices[n_test:]
+    X_train = X[train_indices]
+    X_test = X[test_indices]
+    y_train = y[train_indices]
+    y_test = y[test_indices]
+    
+    return X_train, X_test, y_train, y_test
+
+def z_score_normalization(X):
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0, ddof=0) #ddof=0 for population std , used in SKlearn
+    X_normalized = (X - mean) / std
+    return X_normalized
--- ml_metrics_eval.py
+++ ml_metrics_eval.py
@@ -14,3 +14,28 @@ def root_mean_squared_error(y_true, y_pred):
 def mean_absolute_error(y_true, y_pred):
     mae = np.mean(np.abs(y_true - y_pred))
     return mae
+def accuracy_score(y_true, y_pred):
+    correct_predictions = np.sum(y_true == y_pred)
+    accuracy = correct_predictions / len(y_true)
+    return accuracy
+def precision_score(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    return precision
+def recall_score(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+    return recall
+def f1_score(y_true, y_pred):
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+    return f1
+def confusion_matrix(y_true, y_pred):
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    tn = np.sum((y_true == 0) & (y_pred == 0))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    return np.array([[tn, fp],[fn, tp]])
